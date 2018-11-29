5b43e431899a52c37a2102406b1ee5416e2c1319
Nick Anderegg <nick.anderegg@platform.sh>
2018-11-28 20:32:37 -0500

    Fully functional OAS abstraction with annotations

--- oaspec/spec/schema.py
+++ oaspec/spec/schema.py
@@ -12,35 +12,34 @@ class Schema(object):
     def __init__(self, spec):
         self._raw_spec = spec
 
-        print(self._raw_spec)
-#         jsonschema.validate(self._raw_spec, self._raw_schema)
         if not self.validate(self._raw_spec, True):
             print("VALIDATION ERROR")
 
-#         print("Currently in", self.__class__, self._raw_spec)
+        # If the class has the _boolean_subschema attribute set to something
+        # other than False, detect which definition is present in the parsed
+        # specification and reinitialize the object as the corresponding class
         if self._boolean_subschema:
-#             print("is", self.__class__)
             for subschema_cls in self._boolean_subschema_classes:
-#                 print("Testing subschema:", subschema_cls.__name__)
                 if subschema_cls.validate(self._raw_spec):
-#                     print("correct subschema", subschema_cls.__name__)
-                    self = subschema_cls(self._raw_spec)
-                    break
+                    self.__class__ = subschema_cls
+                    self.__init__(self._raw_spec)
+                    return
+
         elif self._type in self._PRIMITIVES:
             self.value = spec
         elif self._type == "object":
-#             print(self.__class__.__name__)
-#             print(self._raw_schema)
-#             print(self._raw_spec)
             self.set_properties()
         elif self._type == "array":
+            # Create a new object for each item in the array using the class
+            # specified in the _items attribute
             self.value = [self._items(item) for item in spec]
 
     def set_properties(self):
-        if not self._present_properties:
+        if not hasattr(self, "_present_properties"):
             self._present_properties = set()
 
-        # Set named properties
+        # Set named properties by looking at each property present
+        # in the schema definition and checking if it exists in the spec
         for prop, prop_class in self._properties.items():
             if prop in self._raw_spec:
                 self._present_properties.add(prop)
@@ -49,77 +48,235 @@ class Schema(object):
                 if prop in self._required:
                     raise OASpecParserError("Missing required field.", prop)
 
-        # Set patterned properties
-        for prop, prop_class in self._raw_spec.items():
-            if prop[0] == "/":
-                self._present_properties.add(prop)
+        # Set patterned properties by checking each key in the spec that hasn't
+        # already been parsed and checking if it matches the pattern. Create
+        # a new object from the value using the definition specified in the schema
+        for pattern, prop_class in self._pattern_properties.items():
+            for prop, value in self._raw_spec.items():
+                if prop in self._present_properties:
+                    continue
+
+                if self._compiled_patterns[pattern].search(prop):
+                    self._present_properties.add(prop)
+                    setattr(self, prop, prop_class(value))
 
 
-        # Set additional properties
+        # Set additional properties by parsing every key in the spec that
+        # hasn't already been parsed
         if self._additional_properties is not False:
             for prop, value in self._raw_spec.items():
                 if prop in self._present_properties:
-#                     if self.__class__.__name__ == "schemaObject":
                     continue
 
-#                 print("Trying", prop, "in", self.__class__.__name__)
                 self._present_properties.add(prop)
-#                 print(self._additional_properties)
                 setattr(self, prop, self._additional_properties(value))
 
     @classmethod
     def validate(cls, spec, raise_on_failure=False):
+        """Validate a spec with the schema defined in this class.
+
+        This will use the OAS schema stored in the class's `_parsing_schema`
+        attribute to validate the passed specification.
+
+        Parameters:
+            spec: The raw OpenAPI specification to validate.
+            raise_on_failure: Specify whether to raise a ValidationError exception
+                when validation fails. Returns False on validation error if set
+                to False.
+
+        Returns:
+            bool: Returns True if spec validates successfully, returns False on
+                validation failure if `raise_on_failure` is False.
+        """
+
         try:
-            jsonschema.validate(spec, cls._raw_schema)
+            jsonschema.validate(spec, cls._parsing_schema)
             return True
         except jsonschema.ValidationError as e:
             if raise_on_failure:
                 raise e
 
             return False
-        except RecursionError:
-            return True
+        except Exception as e:
+            raise e
 
     def __getattr__(self, name):
+        """Access the computed representation of a specification object.
+
+        Specification objects with a non-mapping type (string, boolean, number,
+        integer, array) store their value in the object's `value` attribute,
+        but objects store their mapping keys as object attributes. Calling
+        `value` on a mapping object will return a dict computed from
+        the properties present in the object as attributes.
+
+        Returns:
+            dict: A dict computed from properties present in the object as attributes.
+        """
         if name == "value":
             if self._type == "object":
                 return {key:getattr(self, key) for key in self._present_properties}
+
+        raise AttributeError(f"Property {name} not present in specification")
 
     def __repr__(self):
         return str(self.value)
 
     def __getitem__(self, key):
-        if self._type != "array":
-            raise TypeError(f"{self.__name__} does not support indexing")
+        if self._type == "object":
+            return getattr(self, key)
+        elif self._type == "array":
+            return self.value[key]
 
-        return self.value[key]
+        raise TypeError(f"{self.__name__} does not support indexing")
 
 def def_key(key):
+    """Compute a definition ref from a key.
+
+    Returns:
+        str: The computed relative reference
+
+    """
     return f"#/definitions/{key}"
 
+def get_all_refs(schema):
+    """Get all ref links in a schema.
+
+    Traverses a schema and extracts all relative ref links from the schema,
+    returning a set containing the results.
+
+    Parameters:
+        schema: An OAS schema in the form of nested dicts to be traversed.
+
+    Returns:
+        set: All of the ref links found during traversal.
+
+    """
+
+    all_refs = set()
+
+    if type(schema) is dict:
+        for key, val in schema.items():
+            if key == "$ref" and type(val) is str:
+                all_refs.add(val)
+
+            all_refs.update(get_all_refs(val))
+    elif type(schema) is list:
+        for item in schema:
+            all_refs.update(get_all_refs(item))
+
+    return all_refs
+
+def get_def_classes(schema, def_objects, ignore_keys=None):
+    """Return the definition objects represented by relative refs in a schema.
+
+    Gets all of the relative refs present in a schema object and returns a mapping
+    of refs to schema objects, recursively resolving references listed in the
+    retrieved schema definitions. This function is used to collect the referenced
+    definitions that will be added to each schema class's `_parsing_schema` attribute.
+
+    Parameters:
+        schema: The schema to parse for relative refs.
+        def_objects: A mapping of relative ref keys and the Schema sub-classes to which they
+            correspond. The `_parsing_schema` will be extracted from each referenced class.
+        ignore_keys: A set of keys that should be skipped when encountered during traversal,
+            in order to prevent infinite recursion when encountering circular references.
+
+    Returns:
+        dict: A mapping of of relative ref keys and their corresponding raw definitions.
+
+    """
+
+    # Traverse the schema to get all relative ref links
+    all_refs = get_all_refs(schema)
+
+    if not all_refs:
+        return {}
+
+    def_classes = {}
+    for key in all_refs:
+        subkey = key.split("/")[-1]
+        if ignore_keys and subkey in ignore_keys:
+            continue
+
+        subschema = deepcopy(def_objects[key]._parsing_schema)
+
+        def_classes[subkey] = subschema
+
+        # Recursively de-reference ref links found in retrieve definition objects
+        def_classes.update(get_def_classes(def_classes[subkey], def_objects, def_classes.keys()))
+
+    return def_classes
+
+def schema_hash(schema):
+    """Generate a string-based hash of a schema object.
+
+    Returns:
+        str: Schema hash.
+
+    """
+    return str(abs(hash(str(schema))))
+
 def build_schema(schema, schema_base, schema_class, object_defs=None):
+    """Recursively build the Schema tree sub-classes for an entire OAS validation schema.
 
+    Parameters:
+        schema: The raw OAS schema used as the metadata source for the created Schema subclass.
+        schema_base: The base class used to build all Schema subclasses. The `Schema` class
+            (or equivalent replacement) should be passed on the first call to this function,
+            while `schema_base` should always be passed when called recursively.
+        schema_class: The subclass being built by this function. This object should be a
+            direct subclass of the Schema class, created by calling the `type` function with
+            a unique name describing the class and the Schema base class.
+        object_defs: The Schema subclasses corresponding to the `definitions` property
+            in an OAS schema. Definition classes are created on the first function call and
+            should be passed directly on recursive calls to this function.
 
+    Returns:
+        Schema: Returns a subclass of the Schema class containing the metadata present
+            in the OAS schema.
+    """
+
+    # If the object is simply a ref link to another definition, return that class directly
+    # rather than generating a new class. Passes the class by reference to be used in-place.
     if schema.keys() == {"$ref": None}.keys():
         return object_defs[schema["$ref"]]
 
-    raw_schema = getattr(schema_class, "_raw_schema", dict())
-    if not raw_schema:
-        schema_class._raw_schema = raw_schema
-    raw_schema.update(schema)
+
+    # Check if Schema already has a raw schema associated with it
+    if getattr(schema_class, "_raw_schema", False):
+        raise RuntimeError("schema_class already has _raw_schema")
+
+    raw_schema = getattr(schema_class, "_raw_schema", {"definitions": dict()})
+    schema_class._raw_schema = raw_schema
+    raw_schema.update(deepcopy(schema))
 
 
+    # Add the raw schema that will be used for schema validation to the metadata.
+    # In contrast to schema_class._raw_schema, the parsing schema must not contain
+    # circular references (i.e. "$ref" links won't be de-referenced in-place).
+    # If this subclass represents an object defined in the `definitions` property
+    # of a schema, the _parsing_schema will already be present.
+    if not hasattr(schema_class, "_parsing_schema"):
+        schema_class._parsing_schema = deepcopy(schema)
+
+
+    # If the object contains a subschema with boolean logic, the function will create
+    # a special-case class that will hold the potential subclasses and re-initialize
+    # itself as one of the subclasses during parsing.
     bool_type = [key for key in schema.keys() if key in {"allOf", "anyOf", "oneOf"}]
     if bool_type:
+
         bool_type = bool_type[0]
         schema_class._boolean_subschema = bool_type
-
         schema_class._boolean_subschema_classes = list()
+
+        # Generate a Schema subclass for each defined subclass, basing the subclass
+        # name on the hash of the definition
         for subschema in schema[bool_type]:
             subschema_name = "".join([
                 schema_class.__name__,
                 "_subschema_",
-                str(abs(hash(str(subschema)))),
+                schema_hash(subschema),
             ])
 
             subschema_class = build_schema(
@@ -132,11 +289,10 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
             schema_class._boolean_subschema_classes.append(subschema_class)
 
         schema_class._raw_schema[bool_type] = [subschema._raw_schema for subschema in schema_class._boolean_subschema_classes]
-
-        return schema_class
     else:
         schema_class._boolean_subschema = False
 
+    # Extract metadata for direct inclusion in created Schema subclass
     if not hasattr(schema_class, "_id"):
         schema_class._id = schema.get("$id", "")
 
@@ -145,23 +301,45 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
     schema_class._type = schema.get("type", "")
     schema_class._required = set(schema.get("required", []))
 
+
     defs = schema.get("definitions", dict())
+    # If the `definitions` property is defined in the OAS schema, process those definitions
     if defs:
+
+        # If `object_defs` has been passed to this function, then we are currently deeper
+        # than the top level of the Schema, and the `definition` property is not currently
+        # supported at lower levels.
+        # TODO: Support definitions at deeper levels of the schema.
+        if object_defs:
+            raise RuntimeError("Definitions are only allowed at top level of spec")
+
+        # Generate a bare Schema subclass for each object defined in `definitions`. The subclass
+        # metadata will be added later, but bare classes are needed to allow recursive referencing
+        # within definition objects.
         schema_class._definitions = {
-            def_key(key):type(key, (Schema,), {"_raw_schema": dict()})
-            for key in defs.keys()
+            def_key(key):type(key, (Schema,), {"_raw_schema": dict(), "_parsing_schema": deepcopy(value)})
+            for key, value in defs.items()
         }
-
-    if not object_defs:
-        object_defs = dict(schema_class._definitions)
+        object_defs = schema_class._definitions
     else:
-        object_defs = dict(object_defs)
+        # Subclasses need to have referenced definitions included in their _parsing_schema
+        # for schema validation to succeed. This extracts the ref links from the schema
+        # and adds the referenced raw objects to the `definitions` of the parsing schema
+        sub_definitions = get_def_classes(schema_class._parsing_schema, object_defs)
+        if sub_definitions:
+            schema_class._parsing_schema["definitions"] = sub_definitions
 
+
+    # After bare classes have been created for all definitions, actually add metadata from schema.
+    # This section will only run at the top level of the schema.
     for key, value in defs.items():
 
+        # Use the existing Schema subclass to allow for passing classes by reference
         subschema_class = schema_class._definitions[def_key(key)]
+
         if "$id" not in value:
             subschema_class._id = def_key(key)
+
         schema_class._definitions[def_key(key)] = build_schema(
             value,
             schema_base,
@@ -169,34 +347,47 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
             object_defs
         )
 
+    # Process the named properties defined in the OAS schema and created a subclass
+    # for the value defined in each mapping
     props = schema.get("properties", dict())
     schema_class._properties = dict()
     for prop, value in props.items():
-        if "$ref" in value:
-            subschema = object_defs[value["$ref"]]
-            schema_class._properties[prop] = subschema
+        # if "$ref" in value:
+        #    subschema = object_defs[value["$ref"]]
+        #    schema_class._properties[prop] = subschema
+        #
+        #    schema_class._raw_schema["properties"][prop] = subschema._raw_schema
+        # else:
 
-            schema_class._raw_schema["properties"][prop] = subschema._raw_schema
-        else:
-            prop_object_name = "".join([
-                schema_class._id.split("/")[-1],
-                "_",
-                prop,
-                "Object"
-            ])
 
-            subschema = build_schema(
-                value,
-                schema_base,
-                type(prop_object_name, (schema_base,), dict()),
-                object_defs
-            )
+        # Create a name for the subclass based on the property name and the object
+        # that it is within.
+        prop_object_name = "".join([
+            schema_class._id.split("/")[-1],
+            "_",
+            prop,
+            "Object"
+        ])
 
-            schema_class._properties[prop] = subschema
-            schema_class._properties[prop]._id = prop_object_name
-            schema_class._raw_schema["properties"][prop] = subschema._raw_schema
+        subschema = build_schema(
+            value,
+            schema_base,
+            type(prop_object_name, (schema_base,), dict()),
+            object_defs
+        )
 
+        schema_class._properties[prop] = subschema
+        schema_class._properties[prop]._id = prop_object_name
+        schema_class._raw_schema["properties"][prop] = subschema._raw_schema
+
+
+    # If the object being parsed is an array type, create a class based
+    # on the subschema present in the "items" key, defaulting to the "any" type.
+    # During parsing, the spec will be passed to the class created here.
     if schema_class._type == "array":
+        if "items" not in schema:
+            schema["items"] = {"$ref": def_key("any")}
+
         items_object_name = "".join([
             schema_class.__name__,
             "_items"
@@ -211,13 +402,17 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
 
         schema_class._raw_schema["items"] = schema_class._items._raw_schema
 
+
+    # Parse the patterned properties listed in the schema and create a Schema
+    # subclass for each.
     pattern_props = schema.get("patternProperties", dict())
     schema_class._pattern_properties = dict()
+    schema_class._compiled_patterns = dict()
     for pattern, value in pattern_props.items():
         pattern_prop_name = "".join([
             schema_class.__name__,
             "_pattern_prop_",
-            str(abs(hash(pattern))),
+            schema_hash(pattern),
         ])
 
         subschema = build_schema(
@@ -228,13 +423,16 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
         )
 
         schema_class._pattern_properties[pattern] = subschema
+        schema_class._compiled_patterns[pattern] = re.compile(pattern)
 
         schema_class._raw_schema["patternProperties"][pattern] = subschema._raw_schema
 
+
+    # Create the subclass that will be used to create additional properties. If
+    # no schema is specified, it will default to "any"
     additional_props = schema.get("additionalProperties", True)
     if additional_props:
         if additional_props is True:
-#             schema_class._additional_properties = def_key("any")
             additional_props = {"$ref": def_key("any")}
 
         additional_props_name = "".join([
@@ -249,7 +447,6 @@ def build_schema(schema, schema_base, schema_class, object_defs=None):
             object_defs,
         )
 
-        schema_class._raw_schema["additionalProperties"] = schema_class._additional_properties._raw_schema
-
+        # schema_class._raw_schema["additionalProperties"] = schema_class._additional_properties._raw_schema
 
     return schema_class
