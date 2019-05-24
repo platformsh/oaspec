26fd8d3f1f9242b9212681d3f793da9a3a2a9c56
Nick Anderegg <nick.anderegg@platform.sh>
2019-05-24 10:25:00 -0400

    Minor changes

--- oaspec/utils/__init__.py
+++ oaspec/utils/__init__.py
@@ -3,12 +3,11 @@
 from ruamel.yaml import YAML, yaml_object
 
 yaml = YAML()
-yaml.width = 100
+yaml.width = 10000
 yaml.preserve_quotes = True
 yaml.map_indent = 2
 yaml.sequence_indent = 4
 yaml.sequence_dash_offset = 2
-yaml.default_flow_style = False
 yaml.allow_duplicate_keys = True
 
 __all__ = {
